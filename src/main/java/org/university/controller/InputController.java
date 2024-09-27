a89818f4aeabc84aee9f60ecb009a458a0d33312
Olesia Kokhman <olesia.kokhman.pz.2021@lpnu.ua>
2024-09-27 18:24:17 +0300

    add custom annotation and validator

--- src/main/java/org/university/controller/InputController.java
+++ src/main/java/org/university/controller/InputController.java
@@ -2,6 +2,7 @@ package org.university.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.university.model.InputData;
 import org.university.model.Randomizer;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -21,7 +23,11 @@ public class InputController {
     }
 
     @PostMapping("/output")
-    public String submitOutput(@ModelAttribute InputData inputData, Model model) {
+    public String submitOutput(@ModelAttribute @Valid InputData inputData, Model model, BindingResult bindingResult) {
+
+        if(bindingResult.hasErrors()) {
+            return "input";
+        }
 
         Randomizer randomizer = new Randomizer(inputData);
         List<Integer> numbers = randomizer.generate();
--- src/main/java/org/university/model/InputData.java
+++ src/main/java/org/university/model/InputData.java
@@ -1,10 +1,26 @@
 package org.university.model;
 
+import org.university.validation.LessThanM;
+
+import javax.validation.constraints.Min;
+
+@LessThanM
 public class InputData {
+
+    @Min(value = 1, message = "m must be > 0")
     private int m;
+
+    @Min(value = 0, message = "a must be >= 0")
+
     private int a;
+
+    @Min(value = 0, message = "c must be >= 0")
     private int c;
+
+    @Min(value = 0, message = "x0 must be >= 0")
     private int x0;
+
+    @Min(value = 1, message = "output number must be > 0")
     private int outputNumber;
 
     public InputData(int m, int a, int c, int x0, int outputNumber) {
