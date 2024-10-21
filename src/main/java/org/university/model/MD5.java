ca1a2576368f11e7aeebf0679a428086065e829e
Olesia Kokhman <olesia.kokhman.pz.2021@lpnu.ua>
2024-10-21 16:06:21 +0300

    add step 3 and create func interface for compression

--- src/main/java/org/university/model/MD5.java
+++ src/main/java/org/university/model/MD5.java
@@ -1,11 +1,14 @@
 package org.university.model;
 
 public class MD5 {
-    //private String initBinaryMessage;
     private String fullBinaryMessage;
 
+    private int A = 0x67452301;
+    private int B = 0xefcdab89;
+    private int C = 0x98badcfe;
+    private int D = 0x10325476;
+
     public MD5(String message) {
-        //this.initBinaryMessage = buildInitBinaryMessage(message);
         fullBinaryMessage = addSupplement(buildInitBinaryMessage(message));
     }
 
@@ -27,32 +30,37 @@ public class MD5 {
 
     private String addSupplement(String message) { // add supplement
 
-        System.out.println("message - " + message);
         StringBuilder builder = new StringBuilder();
         builder.append(message);
         int basicMultiple = 448;
         int fullMultiple = 512;
         int one = 1;
 
-        int remainder = message.length() % fullMultiple; // 320
+        int remainder = message.length() % fullMultiple;
 
         int addNumber = 0;
         if(remainder < basicMultiple) {
-            addNumber = basicMultiple - remainder - one; // 448 - 320 - 1 = 127
+            addNumber = basicMultiple - remainder - one;
         } else { // 456
-            addNumber = (basicMultiple + fullMultiple) - remainder - one; // (448 + 512) - 456 - 1 = 503
+            addNumber = (basicMultiple + fullMultiple) - remainder - one;
         }
 
         builder.append("1");
         builder.append("0".repeat(addNumber));
 
         String binaryLength = String.format("%64s", Integer.toBinaryString(message.length())).replace(' ', '0');
-
-        System.out.println(binaryLength);
         builder.append(binaryLength);
 
         return builder.toString();
     }
+
+    private void compression() {
+        TriFunction<Integer, Integer, Integer, Integer> f = (b, c, d) -> (b & c) | (~b & d);
+        TriFunction<Integer, Integer, Integer, Integer> g = (b, c, d) -> (b & d) | (c & ~d);
+        TriFunction<Integer, Integer, Integer, Integer> h = (b, c, d) -> (b ^ c ^ d);
+        TriFunction<Integer, Integer, Integer, Integer> i = (b, c, d) -> (c ^ (b | ~d));
+    }
+
 
     public String hash() {
         return null;
